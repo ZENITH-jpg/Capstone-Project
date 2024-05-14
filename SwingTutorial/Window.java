90cf59abcc8f307e32ae1cb6bb3fc317521d4745
vanN43 <van.nguyen4@student.tdsb.on.ca>
2024-05-14 10:20:57 -0400

    dttfsdf

--- SwingTutorial/Window.java
+++ SwingTutorial/Window.java
@@ -1,7 +1,32 @@
 import javax.swing.*;
 import java.awt.event.*;
 
-public class Window extends JFrame {
-   private JButton b = new JButton("WOw a button");
    // IntelliJ has a GUI maker
+public class Window extends JFrame {
+   
+   //window components
+	private JLabel l; 
+	private JPanel p;
+	private JButton b;
+	
+	Window(){
+		//initialize the components
+		p = new JPanel();
+		l = new JLabel("Hello world");
+		b = new JButton ("Click me!");
+		
+		p.add (l); //add the label to the panel
+		//b.addActionListener (new TheButton (this)); //add the event listener to the button to make it work
+		p.add(b); //add the button to the panel
+		this.add(p); //add the panel to the frame
+		
+		//set frame properties
+		setResizable (false);
+		setBounds (100, 100, 800, 500);
+		setDefaultCloseOperation (JFrame.EXIT_ON_CLOSE);
+	}
+   public static void main (String[] args) {
+      Window w = new Window();
+      w.setVisible(true);
+   }
 }
