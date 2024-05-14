1bc4f526f70492a489d8f35cc6012c7b5ac08da3
vanN43 <van.nguyen4@student.tdsb.on.ca>
2024-05-14 09:31:21 -0400

    a test window

---
+++ ButtonOne.java
@@ -0,0 +1,10 @@
+import java.awt.event.*;
+
+public class ButtonOne implements ActionListener {
+   Window w;
+   ButtonOne(Window w) {
+      this.w = w;
+   }
+   public void actionPerformed (ActionEvent e) {
+   }
+}
