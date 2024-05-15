d7f5b43abde8aa7f6034c35e46f59a22dadcebd1
vanN43 <van.nguyen4@student.tdsb.on.ca>
2024-05-15 09:22:40 -0400

    update planet

--- Capstone/Planet.java
+++ Capstone/Planet.java
@@ -7,4 +7,21 @@ public class Planet {
       this.blocks = new ArrayList<Block>();
       this.blocks.add(new Block("Rock", 1000));
    }
+   public int findBlock(String type) {
+      for (int i = 0; i < this.blocks.size(); i++) {
+         if (this.blocks.get(i).getType().equals(type))
+            return i;
+      }
+      return -1;
+   }
+   public void addBlock(String type, int volume) {
+      if (findBlock(type) == -1) {
+         this.blocks.add(new Block(type, volume));
+      } else {
+         this.blocks.get(findBlock(type)).addVolume(volume);
+      }
+   }
+   public int getScore() {
+      return this.score;
+   }
 }
