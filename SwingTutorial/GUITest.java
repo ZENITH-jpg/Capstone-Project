26dc3bcca5bebc856e9c252cb73b3317374d28ba
ZENITH-jpg <82682206+ZENITH-jpg@users.noreply.github.com>
2024-05-16 10:22:43 -0400

    GUi testing

--- SwingTutorial/GUITest.java
+++
@@ -1,7 +0,0 @@
-import javax.swing.*;
-
-public class GUITest {
-    private JProgressBar progressBar1;
-    private JPanel panel1;
-    private JButton button1;
-}
--- SwingTutorial/Window.java
+++ SwingTutorial/Window.java
@@ -1,32 +1,48 @@
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.*;
 
    // IntelliJ has a GUI maker
 public class Window extends JFrame {
    
    //window components
-	private JLabel l; 
-	private JPanel p;
-	private JButton b;
+	private JFrame window;
+	public JTextArea message;
+	public JPanel bgPanel[] = new JPanel[10];
+	public JLabel bgLabel[] = new JLabel[10];
 	
 	Window(){
-		//initialize the components
-		p = new JPanel();
-		l = new JLabel("Hello world");
-		b = new JButton ("Click me!");
-		
-		p.add (l); //add the label to the panel
-		//b.addActionListener (new TheButton (this)); //add the event listener to the button to make it work
-		p.add(b); //add the button to the panel
-		this.add(p); //add the panel to the frame
-		
-		//set frame properties
-		setResizable (false);
-		setBounds (100, 100, 800, 500);
-		setDefaultCloseOperation (JFrame.EXIT_ON_CLOSE);
+		window = new JFrame();
+		window.setSize(800,600);
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		window.getContentPane().setBackground(Color.BLACK);
+		window.setLayout(null);
+
+		message = new JTextArea("HAHAHAHAHAHHAHAHA");
+		message.setBounds(50,410,700,150);
+		message.setBackground(Color.black);
+		message.setForeground(Color.white);
+		message.setEditable(false);
+		message.setLineWrap(true);
+		message.setWrapStyleWord(true);
+		message.setFont(new Font("Book Antiqua", Font.PLAIN, 26));
+		window.add(message);
+		createBackground();
+		window.setVisible(true);
+	}
+	public void createBackground(){
+		bgPanel[1] = new JPanel();
+		bgPanel[1].setBounds(50,50,700,350);
+		bgPanel[1].setBackground(Color.blue);
+		bgPanel[1].setLayout(null);
+		window.add(bgPanel[1]);
+
+		bgLabel[1] = new JLabel();
+		bgLabel[1].setBounds(0,0,700,350);
+		bgLabel[1].setBackground(Color.red);
+		bgPanel[1].add(bgLabel[1]);
 	}
    public static void main (String[] args) {
       Window w = new Window();
-      w.setVisible(true);
    }
 }
