10ab63ea4e61f4161790a052a8914fba810f3472
vanN43 <van.nguyen4@student.tdsb.on.ca>
2024-05-16 09:24:12 -0400

    creater warerter

---
+++ Capstone/WaterBlock.java
@@ -0,0 +1,8 @@
+public class WaterBlock extends Block {
+   public WaterBlock(String t, int v) {
+      super(t, v);
+      this.property = "Regulates heat and turns into ice.";
+   }
+   public void doProperty(Planet p) {
+   }
+}
