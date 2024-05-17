dc3e8e52c68b5cec97660421e144767e7d8aef19
vanN43 <van.nguyen4@student.tdsb.on.ca>
2024-05-17 09:19:10 -0400

    Made A GUI

--- src/Window.java
+++ src/Window.java
@@ -4,6 +4,15 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.BoxLayout;
+import javax.swing.SwingConstants;
+import java.awt.Component;
+import java.awt.Color;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class Window extends JFrame {
 
@@ -36,6 +45,33 @@ public class Window extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
+		
+		JPanel panel = new JPanel();
+		contentPane.add(panel);
+		panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+		
+		JLabel lblNewLabel = new JLabel("Menu");
+		lblNewLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
+		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		panel.add(lblNewLabel);
+		
+		JButton btnNewButton = new JButton("Instructions");
+		btnNewButton.setAlignmentX(Component.CENTER_ALIGNMENT);
+		panel.add(btnNewButton);
+		
+		JButton btnLeaderboard = new JButton("Leaderboard");
+		btnLeaderboard.setAlignmentX(Component.CENTER_ALIGNMENT);
+		panel.add(btnLeaderboard);
+		
+		JButton btnNewButton_1_1 = new JButton("Start Game");
+		btnNewButton_1_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				lblNewLabel.setText("HAHAHAHHHAHAHHHAAH");;
+			}
+		});
+		btnNewButton_1_1.setForeground(new Color(0, 255, 0));
+		btnNewButton_1_1.setAlignmentX(Component.CENTER_ALIGNMENT);
+		panel.add(btnNewButton_1_1);
 	}
 
 }
