3b42d5e37975ef581f5171db1da4914b0fd2930a
ZENITH-jpg <82682206+ZENITH-jpg@users.noreply.github.com>
2024-05-17 21:55:33 -0400

    added start menu

--- Capstone/MenuPanel.java
+++ Capstone/MenuPanel.java
@@ -1,18 +1,59 @@
+
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.io.File;
 
-public class MenuPanel extends JPanel {
+public class MenuPanel extends JPanel implements KeyListener {
     Image background;
     Image logo;
+    int index;
+    final String[] options = {"start", "instructions", "leaderboard"};
+    Image[][] menuItems = new Image[3][2];
     MenuPanel(){
+        index = 0;
+        this.setLayout(null);
         this.setBounds(0,0,800,600);
-        logo = new ImageIcon("assets/logo-modified.png").getImage();
+        logo = new ImageIcon("assets/logo-modified.png").getImage(); // text logo for game
         background = new ImageIcon("assets/background.png").getImage(); // get image for background
+        this.addKeyListener(this);
+        for (int i = 0; i < 3; i++) {
+            menuItems[i][0] = new ImageIcon("assets/"+options[i]+".png").getImage();
+            menuItems[i][1] = new ImageIcon("assets/"+options[i]+"active.png").getImage();
+        }
     }
     @Override
-    public void paint(Graphics g){
+    public void paintComponent(Graphics g){
+        super.paintComponents(g);
         g.drawImage(background,0,0, null); // set background to image
-        g.drawImage(logo,180,40,null);
-        g.drawRect(300,200,200,50);
+        g.drawImage(logo,125,40,null);
+        for (int i = 0; i < 3; i++) {
+            if(index == i){
+                g.drawImage(menuItems[i][1],50, 200+80*i,null);
+            }else{
+                g.drawImage(menuItems[i][0],50, 200+80*i,null);
+            }
+        }
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        if(e.getKeyCode() == 83 || e.getKeyCode() == 40){
+            index=(index+1)%3;
+        }
+        else if(e.getKeyCode() == 87 || e.getKeyCode() == 38){
+            index=(index+2)%3;
+        }
+        this.repaint();
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+
     }
 }
--- Capstone/Window.java
+++ Capstone/Window.java
@@ -7,15 +7,21 @@ public class Window extends JFrame {
 
    //window components
 	private JFrame window;
+	private Image icon;
 	public JTextArea message;
 	public JPanel bgPanel[] = new JPanel[10];
 	public JLabel bgLabel[] = new JLabel[10];
 	
 	Window(){
-		window = new JFrame();
+		MenuPanel m = new MenuPanel();
+		icon = new ImageIcon("assets/icon.png").getImage();
+		window = new JFrame("PlanetSim");
+		window.setIconImage(icon);
 		window.setSize(800,600);
+		window.setResizable(false);
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		window.getContentPane().setBackground(Color.white);
+		window.getContentPane().setBackground(Color.black);
+		window.setLayout(null);
 
 		/*message = new JTextArea("HAHAHAHAHAHHAHAHA");
 		message.setBounds(50,410,700,150);
@@ -27,24 +33,23 @@ public class Window extends JFrame {
 		message.setFont(new Font("Book Antiqua", Font.PLAIN, 26));
 		window.add(message);
 		 */
-
-		createBackground();
+		window.addKeyListener(m);
+		window.add(m);
 		window.setVisible(true);
 	}
 	public void createBackground(){
-		/*bgPanel[1] = new JPanel();// add a background (jpanel)
+		/*
+		bgPanel[1] = new JPanel();// add a background (jpanel)
 		bgPanel[1].setBounds(0,0,800,600);
 		bgPanel[1].setLayout(null);
 		window.add(bgPanel[1]); // add background to window
-
 		bgLabel[1] = new JLabel();
 		bgLabel[1].setBounds(0,0,800,600);
 		ImageIcon bgIcon = new ImageIcon(getClass().getClassLoader().getResource("assets/background.png")); // get image for background
 		bgLabel[1].setIcon(bgIcon);
 		bgPanel[1].add(bgLabel[1]); // set background to image
-
 		 */
-		window.add(new MenuPanel());
+
 	}
    public static void main (String[] args) {
       Window w = new Window();
