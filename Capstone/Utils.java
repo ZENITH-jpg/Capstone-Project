973d288990e38b18f46ee021663603bd514e186a
vanN43 <van.nguyen4@student.tdsb.on.ca>
2024-05-21 09:47:29 -0400

    Added mouse working

--- Capstone/Utils.java
+++ Capstone/Utils.java
@@ -3,6 +3,7 @@ import java.awt.*;
 
 public class Utils { // for fonts, custom colors, and other objects that are to be reused
     final static Font MESSAGE_FONT = new Font("Helvetica", Font.PLAIN, 20);
+    final static Font BLOCKTEXT_FONT = new Font("Helvetica", Font.PLAIN, 12);
 
     public static JTextArea messagePanel(String s, int x, int y, int w, int h) {
         JTextArea message = new JTextArea(s);
@@ -15,6 +16,20 @@ public class Utils { // for fonts, custom colors, and other objects that are to 
         message.setLineWrap(true);
         message.setWrapStyleWord(true);
         message.setFont(MESSAGE_FONT);
+        return message;
+    }
+    
+    public static JTextArea blockTextPanel(String s, int x, int y, int w, int h) {
+        JTextArea message = new JTextArea(s);
+        message.setBorder(null);
+        //message.setHighlighter(null);
+        message.setBounds(x, y, w, h);
+        message.setBackground(null);
+        message.setForeground(Color.black);
+        message.setEditable(false);
+        message.setLineWrap(true);
+        message.setWrapStyleWord(true);
+        message.setFont(BLOCKTEXT_FONT);
         return message;
     }
 
