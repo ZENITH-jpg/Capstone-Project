6cb2a36ffad0d3c2982ff3149a03dde0bdb29af9
ZENITH-jpg <82682206+ZENITH-jpg@users.noreply.github.com>
2024-05-22 23:10:10 -0400

    unproductive day

---
+++ Capstone/MazeGame.java
@@ -0,0 +1,23 @@
+import javax.swing.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class MazeGame extends JPanel implements KeyListener {
+    Window window;
+    Planet planet;
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+
+    }
+}
